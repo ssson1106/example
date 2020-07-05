48b6ea2642cfd4a2c2acb4d94f63c85511ab84ec
'ssson1106' <'ssson1106@gmail.com'>
2020-07-05 15:42:26 +0900

    test

--- src/test/java/com/example/TApplicationTests.java
+++ src/test/java/com/example/TApplicationTests.java
@@ -1,16 +1,43 @@
 package com.example;
 
+import java.util.Base64;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.global.security.UserPrincipal;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TApplicationTests {
-
+	
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+	@Value("${app.jwtExpirationInMs}")
+	private String jwtExpirationInMs;
+	
 	@Test
 	public void contextLoads() {
+		String psd = "thswnsgjs";
+		String encPsd = null;
+		
+		encPsd = encoder.encode(psd);
+		
+		System.out.println(encPsd );
+		
+		System.out.println(jwtExpirationInMs);
+		
+		Date now = new Date();
+		System.out.println(now);
+		Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+		System.out.println(expiryDate);
+		
 	}
 
 }
